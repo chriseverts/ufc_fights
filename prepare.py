005ed4aab36d6b32fb349f8f775155e6ff894604
Chris <85950629+chriseverts@users.noreply.github.com>
2021-10-14 13:35:10 -0500

    new edit

--- prepare.py
+++ prepare.py
@@ -12,7 +12,7 @@ from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 
-def prepare_ufc():
+def prepare_ufc(df):
     ''' Prepare ufc data'''
     
     df = new_ufc()
@@ -42,32 +42,7 @@ def prepare_ufc():
      
     # Encode time
 
-    weight_class = df_clean.loc[:,'weight_class']
-    weight_class_list = weight_class.tolist()
-    genderList = []
-    genderCount = [0,0]
-    weight_class_numbers = []
-    weight_class_dict = {"WomenStrawweight":0,"WomenFlyweight":1,"WomenBantamweight":2,"WomenFeatherweight":3,"Flyweight":4,"Bantamweight":5,"Featherweight":6,"Lightweight":7,"Welterweight":8,"Middleweight":9,"LightHeavyweight":10,"Heavyweight":11,"CatchWeight":12,"OpenWeight":13}
-    
-    #print(weight_class_gender)
-    for weights in weight_class_list:
-        if "Women" in weights:
-                genderList.append("f")
-                genderCount[0]+=1
-        else: 
-            genderList.append("m")
-            genderCount[1]+=1
-            weight_class_numbers.append(weight_class_dict[weights])
-
-    genderValues = np.array(genderList)
-    # integer encode
-    label_encoder = LabelEncoder()
-    gender_encoded = label_encoder.fit_transform(genderValues)
-    
-    
-    #Adding gender and weight class into DF as numbers
-    df_clean.insert(0, 'gender', gender_encoded)
-   
+  
     """
 Encoding stances
 "Open Stance":0
@@ -96,7 +71,34 @@ Encoding stances
     df_clean.insert(3, 'B_Stance', b_stanceValues)
     df_clean.insert(4, 'R_Stance', r_stanceValues)
       
-       
+
+    """
+Encoding match results
+Red win = 1, Red lose = 0 & draw = 2
+Did not use label encoder, because it labels in Alphabetical order
+"""
+    match_results = df_clean.loc[:,'Winner']
+    match_results_list = match_results.tolist()
+    matchList = []
+#print(match_results_list)
+    for results in match_results_list:
+        if "Blue" in results:
+            matchList.append("0")
+        elif "Red" in results: 
+            matchList.append("1")
+        else:
+            matchList.append("2")
+        
+    resultValues = np.array(matchList)
+#Dropping previous 'Winner' column
+    df_clean.drop(['Winner'], axis=1, inplace = True)
+#Adding results into DF
+    df_clean.insert(6, 'Winner', resultValues)
+
+#Dropping Draw winners
+    df_clean.drop(df_clean[df_clean['Winner'] == '2' ].index , inplace=True)
+    df_clean ['Winner'] = df_clean.Winner.astype(float)
+
     """
 Encoding Title Bout
 True = 1, False = 0
@@ -119,40 +121,26 @@ Using label encoder
 
     df_clean = df_clean.drop(df_clean.columns[df_clean.columns.str.contains('^B')], axis=1)
     
+    
+    df = df_clean
           
-    return df_clean
+    return df
 
-def train_validate_test(df, target):
-    '''
-    this function takes in a dataframe and splits it into 3 samples, 
-    a test, which is 20% of the entire dataframe, 
-    a validate, which is 24% of the entire dataframe,
-    and a train, which is 56% of the entire dataframe. 
-    It then splits each of the 3 samples into a dataframe with independent variables
-    and a series with the dependent, or target variable. 
-    The function returns train, validate, test sets and also another 3 dataframes and 3 series:
-    X_train (df) & y_train (series), X_validate & y_validate, X_test & y_test. 
-    '''
-    # split df into test (20%) and train_validate (80%)
-    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
+def ufc_split(df):
+    #splitting our data
+    train_validate, test = train_test_split(df, test_size=.2, 
+                                        random_state=123, 
+                                        stratify=df.Winner)
+    train, validate = train_test_split(train_validate, test_size=.3, 
+                                   random_state=123, 
+                                   stratify=train_validate.Winner)
+    return train, validate, test
 
-    # split train_validate off into train (70% of 80% = 56%) and validate (30% of 80% = 24%)
-    train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
 
-        
-    # split train into X (dataframe, drop target) & y (series, keep target only)
-    X_train = train.drop(columns=[target])
-    y_train = train[target]
-    
-    # split validate into X (dataframe, drop target) & y (series, keep target only)
-    X_validate = validate.drop(columns=[target])
-    y_validate = validate[target]
-    
-    # split test into X (dataframe, drop target) & y (series, keep target only)
-    X_test = test.drop(columns=[target])
-    y_test = test[target]
-    
-    return train, validate, test, X_train, y_train, X_validate, y_validate, X_test, y_test
-
+def prep_ufc(df):
+    #cleaning and splitting our data
+    df = prepare_ufc(df)
+    train, validate, test = ufc_split(df)
+    return train, validate, test
 
 
